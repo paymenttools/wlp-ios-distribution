4e86556f3501da7c321c5a361559fc4edc661f01
Andrei Zaharia <andyzaharia@gmail.com>
2025-03-04 16:00:07 +0200

    Switched to unchecked Sendable.

--- Package.swift
+++ Package.swift
@@ -12,6 +12,6 @@ let package = Package(
             targets: ["WhitelabelPaySDK"]),
     ],
     targets: [
-        .binaryTarget(name: "WhitelabelPaySDK", path: "binary/WhitelabelPaySDK-v1.1.0.zip")
+        .binaryTarget(name: "WhitelabelPaySDK", path: "binary/WhitelabelPaySDK-v1.1.1.zip")
     ]
 )
