efc449be7aac2d0104b45df9cb22cb961eb4c911
Andrei Zaharia <andyzaharia@gmail.com>
2025-04-29 22:23:19 +0300

    Added new binary.

--- Package.swift
+++ Package.swift
@@ -12,6 +12,6 @@ let package = Package(
             targets: ["WhitelabelPaySDK"]),
     ],
     targets: [
-        .binaryTarget(name: "WhitelabelPaySDK", path: "binary/WhitelabelPaySDK-v1.1.6.zip")
+        .binaryTarget(name: "WhitelabelPaySDK", path: "binary/WhitelabelPaySDK-v1.1.7.zip")
     ]
 )
