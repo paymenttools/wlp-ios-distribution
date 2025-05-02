c033ffbc3b6fa474d760a2defe47d21bd35affae
Andrei Zaharia <andyzaharia@gmail.com>
2025-05-02 10:06:30 +0300

    fix: Publisher should not get updated in background.

--- Package.swift
+++ Package.swift
@@ -12,6 +12,6 @@ let package = Package(
             targets: ["WhitelabelPaySDK"]),
     ],
     targets: [
-        .binaryTarget(name: "WhitelabelPaySDK", path: "binary/WhitelabelPaySDK-v1.1.7.zip")
+        .binaryTarget(name: "WhitelabelPaySDK", path: "binary/WhitelabelPaySDK-v1.1.8.zip")
     ]
 )
