aa8975f7bf99734d445c1037410880eb23ac6110
Andrei Zaharia <andyzaharia@gmail.com>
2025-05-21 17:13:23 +0300

    Fixed authorisation flag.

--- Package.swift
+++ Package.swift
@@ -12,6 +12,6 @@ let package = Package(
             targets: ["WhitelabelPaySDK"]),
     ],
     targets: [
-        .binaryTarget(name: "WhitelabelPaySDK", path: "binary/WhitelabelPaySDK-v1.1.13.zip")
+        .binaryTarget(name: "WhitelabelPaySDK", path: "binary/WhitelabelPaySDK-v1.1.14.zip")
     ]
 )
