8fbfe72eae2a062373530eeca517550df8af9aef
Andrei Zaharia <andyzaharia@gmail.com>
2025-05-21 18:10:26 +0300

    Updated to 1.1.15

--- Package.swift
+++ Package.swift
@@ -12,6 +12,6 @@ let package = Package(
             targets: ["WhitelabelPaySDK"]),
     ],
     targets: [
-        .binaryTarget(name: "WhitelabelPaySDK", path: "binary/WhitelabelPaySDK-v1.1.14.zip")
+        .binaryTarget(name: "WhitelabelPaySDK", path: "binary/WhitelabelPaySDK-v1.1.15.zip")
     ]
 )
