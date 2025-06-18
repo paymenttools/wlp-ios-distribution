08c647d6d50d22e26e75c4bd9b4c1a44362fefcc
Zaharia Andrei <andyzaharia@gmail.com>
2025-06-18 12:24:09 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             name: "WhitelabelPaySDK", targets: ["WLP"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/andyzaharia/faro-otel-swift-exporter", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/andyzaharia/faro-otel-swift-exporter", exact: "1.0.0"),
     ],
     targets: [
 		.binaryTarget(name: "WhitelabelPaySDK", path: "binary/WhitelabelPaySDK-v1.1.22.zip"),
