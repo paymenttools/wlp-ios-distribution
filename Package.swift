8f3c0d2df7f7ef036b805e109f332c83220a7801
Zaharia Andrei <andyzaharia@gmail.com>
2026-06-15 23:54:33 +0300

    feat: Updated binary for 1.2.12 (#76)

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
 		
     ],
     targets: [
-		.binaryTarget(name: "WhitelabelPaySDK", path: "binary/WhitelabelPaySDK-v1.2.11.zip"),
+		.binaryTarget(name: "WhitelabelPaySDK", path: "binary/WhitelabelPaySDK-v1.2.12.zip"),
 		.target(
 			name: "WLP",
 			dependencies: [
